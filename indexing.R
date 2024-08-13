20b58d93986ce3c23d3156edb204289a0b984f79
kaimorquecho <kaimorquecho@ucsb.edu>
2024-08-13 13:46:33 -0700

    indexing practice

--- indexing.R
+++ indexing.R
@@ -30,6 +30,34 @@ x[c(TRUE, FALSE, NA)] <- -1
 
 df <- data.frame(a = c(1, 10, NA))
 df$a[df$a<5] <- 0
-df$a
+df
 
+mtcars
+mtcars[mtcars$gear == 5,]
+# we have subset our data down ^ down to only those cars that have 5 gears, but we have not saved it to a variable or vector so we are doing this only for vizualization purposes
+
+mtcars[mtcars$gear == 5 & mtcars$cyl == 4,]
+# we can perform this ^ in v this way,too
+subset(mtcars, gear == 5 & cyl == 4)
+
+library(tidyverse)
+ggplot(data = subset(mtcars, gear == 5 & cyl == 4),
+       aes( x = mpg, y = disp))+
+  geom_point()
+
+
+# this is how we can create a df AND remove part of it. we add "letters",which is a constant, bc we are removing letters.
+df <- data.frame(x = 1:3, y = 3:1, z = letters [1:3])
+df 
+df$z <- NULL 
+df
+
+
+# here we are trying to avoid writing out what we want and don't want, goal is to write in less code what we do NOT want
+
+df[c("x","y")] 
+# ^ this line requires us to enter the columns we want to keep
+
+setdiff(names(df), "z")
+# ^ this line requires to enter only the column we want to delete. it asks r to compare column Z with the df and delete its diferences. 
 
